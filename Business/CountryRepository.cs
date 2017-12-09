ba5a63cc9e78aed14de289e6a0f3a08d95716391
martynas5991 <alex_martynas@yahoo.com>
2017-12-09 22:52:58 +0200

    Merged with ilie-andrei

---
+++ Business/CountryRepository.cs
@@ -0,0 +1,40 @@
+﻿using Data.Domain.Entities;
+using Data.Domain.Interfeces;
+using Data.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Business
+{
+    public class CountryRepository : ICountryRepository
+    {
+        private readonly IDatabaseContext _databaseContext;
+
+        public CountryRepository(IDatabaseContext databaseContext)
+        {
+            _databaseContext = databaseContext;
+        }
+
+        public Country GetById(Guid id)
+        {
+            return _databaseContext.Countries.FirstOrDefault(t => t.Id == id);
+        }
+
+        public Country GetByName(string name)
+        {
+            return _databaseContext.Countries.FirstOrDefault(t => t.Name == name);
+        }
+
+        public IReadOnlyList<Country> GetAll()
+        {
+            return _databaseContext.Countries.ToList();
+        }
+
+        public void Add(Country country)
+        {
+            _databaseContext.Countries.Add(country);
+            _databaseContext.SaveChanges();
+        }
+    }
+}
---
+++ Presentation/Controllers/CitiesController.cs
@@ -0,0 +1,31 @@
+﻿using Data.Domain.Entities;
+using Data.Domain.Interfeces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    public class CitiesController : Controller
+    {
+        private readonly ICityRepository _repository;
+
+        public CitiesController(ICityRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public IEnumerable<City> Get()
+        {
+            return _repository.GetAll();
+        }
+
+        [HttpGet("{id:guid}")]
+        public City Get(Guid id)
+        {
+            return _repository.GetById(id);
+        }
+    }
+}
