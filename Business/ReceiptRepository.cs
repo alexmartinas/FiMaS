b711aa717b08651e0678f6e122b5e05062cd96f3
gabrielmiron15 <miron.gabriel2015@gmail.com>
2017-12-12 02:03:51 +0200

    test

---
+++ Business/ReceiptRepository.cs
@@ -0,0 +1,52 @@
+﻿using Data.Domain.Entities;
+using Data.Domain.Interfeces;
+using Data.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Business
+{
+    public class ReceiptRepository : IReceiptRepository
+    {
+        private readonly IDatabaseContext _databaseContext;
+
+        public ReceiptRepository(IDatabaseContext databaseContext)
+        {
+            _databaseContext = databaseContext;
+        }
+
+        IReadOnlyList<Receipt> GetReceiptsByOwner(Guid ownerId)
+        {
+            return _databaseContext.Receipts.ToList();
+        }
+        IReadOnlyList<Receipt> GetReceiptsByPrintedDate(Guid ownerId, DateTime date)
+        {
+            return _databaseContext.Receipts.ToList();
+        }
+        void EditRecceipt(Guid id, Receipt receipt)
+        {
+
+        }
+        void CreateReceipt(Guid ownerId, DateTime printedAt)
+        {
+
+        }
+        void DeleteReceipt(Guid id)
+        {
+
+        }
+        void AddProduct(Guid receiptId, Product product, Guid ownerId)
+        {
+
+        }
+        void DeleteProduct(Guid receiptId, Guid productId)
+        {
+
+        }
+        IReadOnlyList<Receipt> GetById(Guid Id)
+        {
+            return _databaseContext.Receipts.ToList();
+        }
+    }
+}
---
+++ Presentation/Controllers/ReceiptsController.cs
@@ -0,0 +1,25 @@
+﻿using Data.Domain.Entities;
+using Data.Domain.Interfeces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    public class ReceiptsController : Controller
+    {
+        private readonly IReceiptRepository _repository;
+
+        public ReceiptsController(IReceiptRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet("{id:guid}")]
+        public Receipt Get(Guid id)
+        {
+            return _repository.GetById(id);
+        }
+    }
+}
