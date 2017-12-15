8ab74184a2e9a371328fc3ee7ced974ff9de2ade
Alexandru Dochitoiu <alexandru.dochitoiu@info.uaic.ro>
2017-12-16 01:37:01 +0200

    Added DbSeeder for Cities and Countries

---
+++ Presentation/Seeders/CountriesDbSeeder.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Data.Domain.Entities;
+using Data.Persistence;
+
+namespace Presentation.Seeders
+{
+    public class CountriesDbSeeder
+    {
+        private readonly IDatabaseContext _database;
+
+        public CountriesDbSeeder(IDatabaseContext database)
+        {
+            _database = database;
+        }
+
+        public void Seed()
+        {
+                if (_database.Countries.Any()) return;
+                
+                _database.Countries.AddRange(GetCountries());
+                _database.SaveChanges();
+        }
+
+        private static IEnumerable<Country> GetCountries()
+        {
+            return new List<Country>
+            {
+                Country.Create("Romania")
+            };
+        }
+    }
+}
