f86098ee86b1c1da331a97032d573a2def6d54ee
gabrielmiron15 <miron.gabriel2015@gmail.com>
2017-12-19 00:24:01 +0200

    merge register

--- Presentation/ClientApp/components/Register.tsx
+++ Presentation/ClientApp/components/Register.tsx
@@ -111,6 +111,7 @@ export class Register extends React.Component<RouteComponentProps<{}>, IRegister
                                     onChange={this.onChange}
                                     placeholder="Your Name"
                                     name="name"
+                                    required
                                     type="text" />
                                 <div className="input-icon"><i className="glyphicon glyphicon-user"></i></div>
                             </div>
@@ -124,6 +125,7 @@ export class Register extends React.Component<RouteComponentProps<{}>, IRegister
                                     onChange={this.onChange}
                                     placeholder="Email Address"
                                     name="email"
+                                    required
                                     type="email" />
                                 <div className="input-icon"><i className="glyphicon glyphicon-envelope"></i></div>
                             </div>
@@ -135,6 +137,7 @@ export class Register extends React.Component<RouteComponentProps<{}>, IRegister
                                 <input
                                     value={this.state.password}
                                     onChange={this.onChange}
+                                    required
                                     placeholder="Password"
                                     name="password"
                                     type="password" />
@@ -157,14 +160,14 @@ export class Register extends React.Component<RouteComponentProps<{}>, IRegister
                     </div>
                 </div>
                 <div className="row">
-                    <div className="col-xs-6">
+                    <div className="col-xs-12 col-sm-6">
                         <div className="input-group country-box">
                             {
                                 this.renderCountries()
                             }
                         </div>
                     </div>
-                   <div className="col-xs-6">
+                   <div className="col-xs-6 col-sm-6">
                         <div className="input-group">
                             {
                                 this.renderCities()
